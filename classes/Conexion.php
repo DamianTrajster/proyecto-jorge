27ef1293f67c8f42af7be47d69e21535be1ee52b
DamianTrajster <damiantrajster@gmail.com>
2023-10-21 12:58:36 -0300

    second commit

--- classes/Conexion.php
+++ classes/Conexion.php
@@ -16,7 +16,7 @@ class Conexion {
 
         try {
             $dsn = "mysql:host=$dbServer;dbname=$dbName;charset=utf8mb4";
-            $this->db = new PDO($dsn, $dbUser,$dbPass,$dbPort);
+            $this->db = new PDO($dsn, $dbUser, $dbPass,[]);
         } catch (PDOException $e) {
             die("Error al conectar con MySQL: " . $e->getMessage());
         }
